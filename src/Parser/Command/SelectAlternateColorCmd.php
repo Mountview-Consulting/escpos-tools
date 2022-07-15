a8082499b34b331b8ee3c634584255a53e49cf54
mackfish-1 <77241706+mackfish-1@users.noreply.github.com>
2022-07-15 08:53:37 +0100

    Update SelectAlternateColorCmd.php

--- src/Parser/Command/SelectAlternateColorCmd.php
+++ src/Parser/Command/SelectAlternateColorCmd.php
@@ -2,8 +2,21 @@
 namespace ReceiptPrintHq\EscposTools\Parser\Command;
 
 use ReceiptPrintHq\EscposTools\Parser\Command\CommandOneArg;
+use ReceiptPrintHq\EscposTools\Parser\Command\InlineFormattingCmd;
+use ReceiptPrintHq\EscposTools\Parser\Context\InlineFormatting;
 
-class SelectAlternateColorCmd extends CommandOneArg
+class SelectAlternateColorCmd extends CommandOneArg implements InlineFormattingCmd
 {
 
+    public function applyToInlineFormatting(InlineFormatting $formatting)
+    {
+		$arg = $this -> getArg();
+		if ($arg === 1) {
+			$formatting -> setColor2(1);
+		} else {
+			$formatting -> setColor2(0);
+		}
+
+    }
 }
+
